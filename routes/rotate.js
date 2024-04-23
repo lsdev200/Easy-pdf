2a3b28dbf39513b1ae334874b78873382e90992b
lsdev200 <lsdev200@gmail.com>
2024-04-23 17:51:10 +0530

    fixing

--- routes/rotate.js
+++ routes/rotate.js
@@ -1,30 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { degrees } = require('pdf-lib');
-const { PDFDocument, rgb } = require('pdf-lib');
-const multer = require('multer');
+const { degrees } = require("pdf-lib");
+const { PDFDocument, rgb } = require("pdf-lib");
+const multer = require("multer");
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.get('/rotate-pdf', (req, res) => {
-    res.render('rotate');
+router.get("/rotate-pdf", (req, res) => {
+  res.render("rotate");
 });
 
-router.post('/rotate-pdf-form', upload.single('file'), async (req, res) => {
+router.post("/rotate-pdf-form", upload.single("file"), async (req, res) => {
   try {
     const rotationDirection = req.body.rotationDirection;
     let rotationDegrees = 0;
 
     switch (rotationDirection) {
-      case 'left':
+      case "upsideDown":
+        rotationDegrees += 180;
+        break;
+      case "left":
         rotationDegrees += 270;
         break;
-      case 'right':
+      case "right":
         rotationDegrees += 90;
-        break;
-      case 'upsideDown':
-        rotationDegrees += 180;
         break;
     }
 
@@ -37,15 +37,14 @@ router.post('/rotate-pdf-form', upload.single('file'), async (req, res) => {
 
     const pdfBytes = await pdfDoc.save();
 
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', `attachment; filename=rotated.pdf`);
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", `attachment; filename=rotated.pdf`);
 
     res.send(Buffer.from(pdfBytes));
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error rotating PDF');
+    res.status(500).send("Error rotating PDF");
   }
 });
 
-  
 module.exports = router;
